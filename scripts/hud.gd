8d868311e640773625e21c9debdf4a6b1d65f73c
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-11-21 18:27:08 +0000

    Reconstructed HUD, moved PauseMenu into it

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,26 +3,32 @@ extends Control
 var level_shown = false
 var paused = false
 var mainMenu_shown = false
-var buttonsForSelection = [$PlayButton,$CreditsButton] # should contain all buttons in order from top to bottom
+var buttonsForSelection = ["PlayButton","CreditsButton"] # should contain all buttons in order from top to bottom
 var buttonSelectedIndex = 0
-
-
+signal playButtonPressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ButtonSelectors.position = buttonsForSelection[buttonSelectedIndex].position.y
+	$ButtonSelectors.hide()
+	$ButtonSelectors.position.y = $PlayButton.position.y
+	$ButtonSelectors.show()
 	
-	
+func moveButtonsToNodeY(selection,index):
+	$ButtonSelectors.position.y = get_node(selection[index]).position.y
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("arrow_down") and mainMenu_shown: # if we are navigating the HUD
 		buttonSelectedIndex += 1
 		buttonSelectedIndex = buttonSelectedIndex % len(buttonsForSelection)
-		#moveButtonSelectorsToButton(buttonsForSelection,buttonSelectedIndex)
+		moveButtonsToNodeY(buttonsForSelection,buttonSelectedIndex)
 	elif Input.is_action_just_pressed("arrow_up") and mainMenu_shown:
 		buttonSelectedIndex -= 1
 		buttonSelectedIndex = buttonSelectedIndex % len(buttonsForSelection)
-		#moveButtonSelectorsToButton(buttonsForSelection,buttonSelectedIndex)
+		moveButtonsToNodeY(buttonsForSelection,buttonSelectedIndex)
+	elif Input.is_action_just_pressed("select") and mainMenu_shown:
+		if buttonsForSelection[buttonSelectedIndex] == "PlayButton":
+			playButtonPressed.emit()
 
 func _on_main_level_shown() -> void:
 	level_shown = true
--- scripts/main.gd
+++ scripts/main.gd
@@ -39,3 +39,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_hud_play_button_pressed() -> void:
+	level_shown.emit()
