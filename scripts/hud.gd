a713a548fc98e7ea599db9d0501b441f4d1543d8
Goldenfootie <tommygill789@gmail.com>
2025-11-22 17:51:53 +0000

    Update HUD
    Makes the HUD work, adds buttons to the pause menu and makes scripts involved more legible.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,37 +1,124 @@
 extends Control
 
+#Variables
+#Booleans
 var level_shown = false
 var paused = false
 var mainMenu_shown = false
-var mainMenubuttonsForSelection = ["MainMenu/PlayButton","MainMenu/CreditsButton"] # should contain all buttons in order from top to bottom
+#Lists
+var mainMenubuttonsForSelection = ["MainMenu/ButtonsContainer/PlayButton","MainMenu/ButtonsContainer/CreditsButton","MainMenu/ButtonsContainer/ExitGameButton"] # should contain all main menu buttons in order from top to bottom
+var pauseMenubuttonsForSelection = ["PauseMenu/ResumeButton","PauseMenu/OptionsButton","PauseMenu/ExitToMenuButton","PauseMenu/ExitGameButton"] # should contain all pause menu buttons in order from top to bottom
+#Integers
 var mainMenubuttonSelectedIndex = 0
-signal playButtonPressed
-signal pausePressed
+var pauseMenubuttonSelectedIndex = 0
 
+#Signals
+signal playButtonPressed
+signal creditsButtonPressed
+signal pausePressed
+signal exitGameButtonPressed
+signal resumeButtonPressed
+signal exitToMenuButtonPressed
+signal optionsButtonPressed
+
+#Main Code:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MainMenu/ButtonSelectors.hide()
-	$MainMenu/ButtonSelectors.position.y = $MainMenu/PlayButton.position.y
+	$MainMenu/ButtonSelectors.position.y = $MainMenu/ButtonsContainer/PlayButton.position.y + get_node('MainMenu/ButtonsContainer').position.y #Calculates the global height of the buttons
 	$MainMenu/ButtonSelectors.show()
 	
-func moveButtonsToNodeY(selection,index):
-	$MainMenu/ButtonSelectors.position.y = get_node(selection[index]).position.y
-
+	#Set booleans to correct values
+	if get_node('MainMenu').is_visible_in_tree():
+		mainMenu_shown = true
+	elif get_node('PauseMenu').is_visible_in_tree():
+		paused = true
+	elif false:
+		#Replace this with any other checks to set booleans
+		pass
+	else:
+		level_shown = true
+	
+func moveMainButtonsToNodeY(selection,index):
+	$MainMenu/ButtonSelectors.position.y = get_node(selection[index]).position.y + get_node('MainMenu/ButtonsContainer').position.y #Calculates the global height of the buttons
+func movePauseButtonsToNodeY(selection,index):
+	$PauseMenu/ButtonSelector.position.y = get_node(selection[index]).position.y #Calculates the global height of the buttons
+	$PauseMenu/ButtonSelector.position.x = get_node(selection[index]).position.x - 29 #Calculates the necessary x position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("arrow_down") and mainMenu_shown: # if we are navigating the HUD
-		mainMenubuttonSelectedIndex += 1
-		mainMenubuttonSelectedIndex = mainMenubuttonSelectedIndex % len(mainMenubuttonsForSelection)
-		moveButtonsToNodeY(mainMenubuttonsForSelection,mainMenubuttonSelectedIndex)
-	elif Input.is_action_just_pressed("arrow_up") and mainMenu_shown:
-		mainMenubuttonSelectedIndex -= 1
-		mainMenubuttonSelectedIndex = mainMenubuttonSelectedIndex % len(mainMenubuttonsForSelection)
-		moveButtonsToNodeY(mainMenubuttonsForSelection,mainMenubuttonSelectedIndex)
-	elif Input.is_action_just_pressed("select") and mainMenu_shown:
-		if mainMenubuttonsForSelection[mainMenubuttonSelectedIndex] == "PlayButton":
-			playButtonPressed.emit()
-	if Input.is_action_just_pressed("escape") and level_shown:
-		pausePressed.emit()
+	if mainMenu_shown: #If we are on the MainMenu
+		if Input.is_action_just_pressed("arrow_down"):
+			mainMenubuttonSelectedIndex += 1
+			mainMenubuttonSelectedIndex = mainMenubuttonSelectedIndex % len(mainMenubuttonsForSelection)
+			moveMainButtonsToNodeY(mainMenubuttonsForSelection,mainMenubuttonSelectedIndex)
+		elif Input.is_action_just_pressed("arrow_up"):
+			mainMenubuttonSelectedIndex -= 1
+			mainMenubuttonSelectedIndex = mainMenubuttonSelectedIndex % len(mainMenubuttonsForSelection)
+			moveMainButtonsToNodeY(mainMenubuttonsForSelection,mainMenubuttonSelectedIndex)
+		elif Input.is_action_just_pressed("select"):
+			if mainMenubuttonsForSelection[mainMenubuttonSelectedIndex] == "MainMenu/ButtonsContainer/PlayButton":
+				playButtonPressed.emit()
+				print('playButtonPressed')
+				level_shown = true
+				mainMenu_shown = false
+				get_node('MainMenu').hide()
+			elif mainMenubuttonsForSelection[mainMenubuttonSelectedIndex] == "MainMenu/ButtonsContainer/CreditsButton":
+				creditsButtonPressed.emit()
+				print('creditsButtonPressed')
+			elif mainMenubuttonsForSelection[mainMenubuttonSelectedIndex] == "MainMenu/ButtonsContainer/ExitGameButton":
+				exitGameButtonPressed.emit()
+				print('exitGameButtonPressed')
+				get_tree().quit()
+			
+		
+	
+	elif paused: #If we are on the pause menu
+		if Input.is_action_just_pressed("escape"):
+			resumeButtonPressed.emit()
+			print('resumePressed')
+			level_shown = true
+			paused = false
+			get_node('PauseMenu').hide()
+		if Input.is_action_just_pressed("arrow_down"):
+			pauseMenubuttonSelectedIndex += 1
+			pauseMenubuttonSelectedIndex = pauseMenubuttonSelectedIndex % len(pauseMenubuttonsForSelection)
+			movePauseButtonsToNodeY(pauseMenubuttonsForSelection,pauseMenubuttonSelectedIndex)
+		elif Input.is_action_just_pressed("arrow_up"):
+			pauseMenubuttonSelectedIndex -= 1
+			pauseMenubuttonSelectedIndex = pauseMenubuttonSelectedIndex % len(pauseMenubuttonsForSelection)
+			movePauseButtonsToNodeY(pauseMenubuttonsForSelection,pauseMenubuttonSelectedIndex)
+		elif Input.is_action_just_pressed("select"):
+			if pauseMenubuttonsForSelection[pauseMenubuttonSelectedIndex] == "PauseMenu/ResumeButton":
+				resumeButtonPressed.emit()
+				print('resumeButtonPressed')
+				level_shown = true
+				paused = false
+				get_node('PauseMenu').hide()
+			elif pauseMenubuttonsForSelection[pauseMenubuttonSelectedIndex] == "PauseMenu/OptionsButton":
+				optionsButtonPressed.emit()
+				print('optionsButtonPressed')
+			elif pauseMenubuttonsForSelection[pauseMenubuttonSelectedIndex] == "PauseMenu/ExitToMenuButton":
+				exitToMenuButtonPressed.emit()
+				print('exitToMenuButtonPressed')
+				paused = false
+				mainMenu_shown = true
+				get_node('MainMenu').show()
+				get_node('PauseMenu').hide()
+			elif pauseMenubuttonsForSelection[pauseMenubuttonSelectedIndex] == "PauseMenu/ExitGameButton":
+				exitGameButtonPressed.emit()
+				print('exitGameButtonPressed')
+				get_tree().quit()
+			
+		
+	elif level_shown:
+		if Input.is_action_just_pressed("escape"):
+			pausePressed.emit()
+			print('pausePressed')
+			level_shown = false
+			paused = true
+			get_node('PauseMenu').show()
+		
+	
 
 func _on_main_level_shown() -> void:
 	level_shown = true
